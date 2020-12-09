a77843aba3af0b1370ec0df57265b175f9e73c2c
PixelSergey <sergey.ichtchenko@gmail.com>
2020-12-09 14:39:08 +0200

    Add login/logout functionality

--- totallysafewebsite/urls.py
+++ totallysafewebsite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", LoginView.as_view(template_name="login.html")),
+    path("logout/", LogoutView.as_view(next_page="/")),
     path("", include("vulns.urls"))
 ]
