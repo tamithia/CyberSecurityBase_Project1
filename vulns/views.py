017add1584343833ad4d503237c4ec867f108bd7
PixelSergey <sergey.ichtchenko@gmail.com>
2020-12-25 14:25:50 +0200

    Fix unintentional vulnerability
    The code would delete all users' notes, not just the one logged in

--- vulns/views.py
+++ vulns/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Note
 
 
@@ -8,14 +9,15 @@ from .models import Note
 def index(request):
     notes = Note.objects.filter(owner=request.user)
     rendernotes = ['<span style="color: {}"><li>{}</li></span>'.format(note.colour, note.text) for note in notes]
-    return render(request, "index.html", {"notes":rendernotes})
+    return render(request, "index.html", {"notes":rendernotes,"currentuser":request.user})
 
 
 @login_required
 def addnote(request):
+    user = User.objects.get(username=request.GET.get("user"))
     text = request.GET.get("note")
     colour = request.GET.get("colour")
-    note = Note(owner=request.user, text=text, colour=colour)
+    note = Note(owner=user, text=text, colour=colour)
     note.save()
     return redirect("/")
 
@@ -25,7 +27,7 @@ def deletenote(request):
     text = request.GET.get("note")
     if not text:
         return redirect("/")
-    notes = Note.objects.raw("SELECT * FROM vulns_note WHERE owner = {} AND text LIKE '%{}%'".format(request.user, text))
+    notes = Note.objects.raw("SELECT * FROM vulns_note WHERE owner_id = {} AND text LIKE '%{}%'".format(request.user.id, text))
     for note in notes:
         note.delete()
     return redirect("/")
